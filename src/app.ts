0086c6e7b9e67138b60e2b2b6180f5fcb8b8166b
CamilleAbella <camille.abella@hotmail.fr>
2021-05-15 18:22:15 +0200

    sync native files

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 // ⛔ This file is auto-generated, please don't edit it
 export * from "discord.js"
 export * from "./app.native"
-export * from "./namespaces/utils"
+export * from "./namespaces/utils.native"
 export * from "./namespaces/docs"
 
 export * as Discord from "discord.js"
-export * as utils from "./namespaces/utils"
+export * as utils from "./namespaces/utils.native"
 export * as docs from "./namespaces/docs"
--- src/namespaces/utils.native.ts
+++ src/namespaces/utils.native.ts
@@ -1,20 +1,17 @@
 import Discord from "discord.js"
 
-import guilds from "../tables/guilds"
+import guilds from "../tables/guilds.native"
 
 export async function prefix(guild?: Discord.Guild): Promise<string> {
   let prefix = process.env.PREFIX as string
-
   if (guild) {
     const guildData = await guilds.query
       .where("id", guild.id)
       .select("prefix")
       .first()
-
     if (guildData) {
       return guildData.prefix ?? prefix
     }
   }
-
   return prefix
 }
